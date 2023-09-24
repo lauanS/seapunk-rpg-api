6efdeabf372994ad6d9dc7306ebf55cc35b926da
A-nita <nimoura1@gmail.com>
2023-09-24 03:39:56 -0300

    typescript e formato de diretório adicionados

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Seapunk');
+  res.send('Seapunk 1.0');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
