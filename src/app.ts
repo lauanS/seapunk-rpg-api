14c07ea1717ec6f44f62bb538176469fe5409c90
lauanS <lauan.s.souza@gmail.com>
2023-10-04 19:44:50 -0300

    Separando app e server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,3 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-const envPath = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`);
-
-dotenv.config({
-  path: envPath
-});
-
 import express from 'express';
 import dbConnect from './config/dbConnect';
 import routes from './routes/index';
@@ -17,6 +8,5 @@ dbConnect();
 
 const app = express();
 routes(app);
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
