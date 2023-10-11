f9d2220c588bddd7215f22c94108fb81539d6763
lauanS <lauan.s.souza@gmail.com>
2023-10-11 18:35:01 -0300

    Validação de campos (zod)

--- src/routes/character/create/index.ts
+++ src/routes/character/create/index.ts
@@ -1,20 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
 
 export default class CreateCharacterRoute {
-  static controller (_req: Request, res: Response): void {
-    res.status(200).jsonp({ success: true, result: 'Personagem cadastrado' });
+  static controller (req: Request, res: Response): void {
+    res.status(200).jsonp({ success: true, result: req.body });
   }
 
   static validation (req: Request, res: Response, next: NextFunction): void {
     try {
-      const characterInfo = req.body;
+      const schemaValidator = z.object({
+        name: z.string(),
+        race: z.string(),
+        class: z.array(z.object(
+          {
+            name: z.string(),
+            level: z.number()
+          }
+        )),
+        origin: z.string(),
+        deity: z.string(),
+        level: z.number(),
+      });
 
-      console.log(characterInfo);
+      const bodyParsed = schemaValidator.safeParse(req.body);
+
+      if (!bodyParsed.success) {
+        throw 'As informações fornecidas são inválidas';
+      }
 
       next();
     } catch (error) {
-      console.log('Ocorreu um erro:', error);
-      res.status(200).jsonp({ success: false, result: 'Ocorreu um erro' });
+      res.status(200).jsonp({ success: false, result: typeof error === 'string' ? error : 'Ocorreu um erro' });
     }
   }
 }
