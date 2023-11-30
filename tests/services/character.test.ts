8dc88f981f40bf2eccb3b9a7b3cf5a8847f67078
lauanS <lauan.s.souza@gmail.com>
2023-11-30 00:40:55 -0300

    Melhoria no teste de criação de personagem
    Vericando se foi inserido, utilizando a listagem

--- tests/services/character.test.ts
+++ tests/services/character.test.ts
@@ -1,35 +1,52 @@
 import CreateCharacterService from '@/services/character/create';
-import { expect, test } from 'vitest';
+import ListCharacterService from '@/services/character/list';
+import CharacterRepository from '@/repositories/inMemoryDb/character';
 
-test('Create a character', async () => {
-  const db = [];
-  const createCharacterRepository = {
-    create: (params: createCharacterParams) => {
-      return new Promise<unknown>((resolve) => {
-        db.push(params);
-        resolve(params);
-      });
-    },
-    list: () => new Promise<void>((resolve) => resolve())
-  };
+import { describe, expect, test } from 'vitest';
 
-  const params = {
-    name: 'Francisco',
-    race: 'Kliren',
-    class: [{
-      name: 'Inventor',
-      level: 1
-    }],
-    origin: 'Marine',
-    deity: 'Tannah Tho',
+const defaultCharacter = {
+  name: 'Francisco',
+  race: 'Kliren',
+  class: [{
+    name: 'Inventor',
     level: 1
-  };
+  }],
+  origin: 'Marine',
+  deity: 'Tannah Tho',
+  level: 1
+};
 
-  const createCharacterService = new CreateCharacterService(createCharacterRepository);
+describe('Create character', () => {
+  test('Basic case', async () => {
+    const characterRepository = new CharacterRepository();
+    const createCharacterService = new CreateCharacterService(characterRepository);
 
-  expect(createCharacterService).toBeInstanceOf(CreateCharacterService);
+    expect(createCharacterService).toBeInstanceOf(CreateCharacterService);
 
-  const serviceResult = await createCharacterService.execute(params);
+    const serviceResult = await createCharacterService.execute(defaultCharacter);
 
-  expect(serviceResult).toBe('Personagem cadastrado com sucesso');
+    expect(serviceResult).toBe('Personagem cadastrado com sucesso');
+  });
+
+  test('Check insertions', async () => {
+    /* Setup */
+    const characterRepository = new CharacterRepository();
+    const createCharacterService = new CreateCharacterService(characterRepository);
+    const listCharacterService = new ListCharacterService(characterRepository);
+
+    /* First insertion */
+    await createCharacterService.execute(defaultCharacter);
+
+    let listedCharacters = (await listCharacterService.execute()) as Array<unknown>;
+
+    expect(listedCharacters).instanceOf(Array);
+    expect(listedCharacters.length).equals(1);
+
+    /* Second insertion */
+    await createCharacterService.execute(defaultCharacter);
+    listedCharacters = (await listCharacterService.execute()) as Array<unknown>;
+
+    expect(listedCharacters).instanceOf(Array);
+    expect(listedCharacters.length).equals(2);
+  });
 });
