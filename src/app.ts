bc0dd0a27438cb94075606a1de315b3cc1a4ed27
lauanS <lauan.s.souza@gmail.com>
2024-03-18 22:00:52 -0300

    Adicionado middleware do cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import dbConnect from '@/config/dbConnect';
 import routes from '@/routes';
-
 const PORT = process.env.API_PORT;
 
 dbConnect();
 
 const app = express();
+
+app.use(cors());
 routes(app);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
