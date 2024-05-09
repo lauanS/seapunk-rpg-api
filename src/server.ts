a84b918b4065de9ec690237aca49bb0f1a52768e
lauanS <lauan.s.souza@gmail.com>
2024-05-09 18:27:29 -0300

    Criação de um arquivo de configuração para variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,2 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-const envPath = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`);
-
-dotenv.config({
-  path: envPath
-});
-
+import '@/config/env';
 import './app';
